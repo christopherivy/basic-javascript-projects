a014db18737d5b23de7dd362d672dd9b72d08b81
christopherivy <cdi.747@gmail.com>
2022-07-20 11:07:07 -0700

    calculator

---
+++ calculator/script.js
@@ -0,0 +1,29 @@
+const btns = document.querySelectorAll('.btn');
+const screen = document.querySelector('.screen');
+const equalBtn = document.querySelector('.btn-equal');
+const clearBtn = document.querySelector('.btn-clear');
+
+
+
+for(let i = 0; i < btns.length; i++) {
+	btns[ i ].addEventListener('click', function () {
+		let number = btns[ i ].getAttribute('data-num');
+
+		screen.value += number;
+	});
+}
+
+equalBtn.addEventListener('click', function () {
+	if(screen.value === '') {
+		equalBtn.getElementById('btn-equal').disabled = true;
+	} else {
+
+		let value = eval(screen.value);
+		screen.value = value;
+	}
+
+});
+
+clearBtn.addEventListener('click', function () {
+	screen.value = '';
+});
